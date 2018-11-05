b6239bcc37456dc4d901a66fbfa83eafd2fa5464
drajkumar <daniel.rajkumar@ascenaretail.com>
2018-11-05 17:57:09 +0530

    application.conf file added

--- src/test/scala/XCCPEMCall.scala
+++ src/test/scala/XCCPEMCall.scala
@@ -1,32 +1,42 @@
 package test.scala
 
+import com.typesafe.config.ConfigFactory
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+
 import scala.concurrent.duration._
 
 class XCCPEMCall extends Simulation {
 
+  val hostName = ConfigFactory.load().getString("application.pem.hostname")
+  val protocol = ConfigFactory.load().getString("application.pem.protocol")
+  val port = ConfigFactory.load().getString("application.pem.port")
+
   val httpProtocol = http
-    .baseUrl("http://718724-LBXCCSITpmm1.ascenaretail.com:8091")
+    .baseUrl(protocol + "://" + hostName + ":" + port)
     .inferHtmlResources()
     .acceptEncodingHeader("gzip,deflate")
     .contentTypeHeader("text/xml;charset=UTF-8")
     .userAgentHeader("Apache-HttpClient/4.1.1 (java 1.5)")
 
+  val pemEndPointURL = ConfigFactory.load().getString("application.pem.endpointURL")
+  val pemPayLoadPath = ConfigFactory.load().getString("application.pem.payLoadPath")
   object PEM {
     val pemCalls = exec(http("XCC PEM Call Testing")
-      .post("/PromotionEngineWebServiceWrapper/PromotionEngineWebServiceWrapper?wsdl")
-      .body(ElFileBody("PEMRequest.xml"))
+      .post(pemEndPointURL)
+      .body(ElFileBody(pemPayLoadPath))
      .check(
         status.is(200),
         regex("soapFault").notExists
       ))
   }
 
+  val ccmEndPointURL = ConfigFactory.load().getString("application.ccm.endpointURL")
+  val ccmPayLoadPath = ConfigFactory.load().getString("application.ccm.payLoadPath")
   object CCM {
     val ccmCalls = exec(http("XCC CCM Call Testing")
-      .get("/CouponManagement/CouponWS?WSDL")
-      .body(ElFileBody("CCMRequest.xml"))
+      .get(ccmEndPointURL)
+      .body(ElFileBody(ccmPayLoadPath))
       .check(
         status.is(200),
         regex("soapFault").notExists
@@ -36,6 +46,6 @@ class XCCPEMCall extends Simulation {
   val xccScenario = scenario("XCCSoapCallsSimulation").exec(PEM.pemCalls, CCM.ccmCalls)
 
   setUp(
-      xccScenario.inject(rampUsers(300) during (60 seconds)),
+      xccScenario.inject(rampUsers(1) during (1 seconds)),
   ).protocols(httpProtocol)
 }
